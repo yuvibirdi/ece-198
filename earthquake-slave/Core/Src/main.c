e555fbd81025b48ffe593b2bf2576e78146758b6
vintaninja <vintaninja@gmail.com>
2024-11-22 14:32:10 -0500

    Support for 1 accelerometer, only PB8 and 9 are "turned on" in IOC file.

--- earthquake-slave/Core/Src/main.c
+++ earthquake-slave/Core/Src/main.c
@@ -18,67 +18,168 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include <stdio.h>
-#include <stdbool.h>
-#define DEVICE_ADDRESS 0x68
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
 /* USER CODE END Includes */
-
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
 /* USER CODE END PTD */
-
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define MPU6050_ADDR (0x68 << 1)
 
+#define FS_GYRO_250 0
+#define FS_GYRO_500 8
+
+
+#define FS_ACC_2G 0
+#define FS_ACC_4G 8
+
+// Other registers
+#define REG_CONFIG_GYRO 27
+#define REG_CONFIG_ACC 28
+#define REG_USR_CTRL 107 // 0x6B is PWR_MGMT_1 register
+#define REG_DATA 59
+
+// ACCEL registers
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_XOUT_L 0x3C
+
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_YOUT_L 0x3E
+
+#define ACCEL_ZOUT_H 0x3F
+#define ACCEL_ZOUT_L 0x40
 /* USER CODE END PD */
-
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
-
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
-I2C_HandleTypeDef hi2c3;
-
-SPI_HandleTypeDef hspi2;
-SPI_HandleTypeDef hspi3;
-
 UART_HandleTypeDef huart2;
-
 /* USER CODE BEGIN PV */
-
 /* USER CODE END PV */
-
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
-static void MX_I2C3_Init(void);
-static void MX_SPI2_Init(void);
-static void MX_SPI3_Init(void);
 /* USER CODE BEGIN PFP */
-
 /* USER CODE END PFP */
-
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-/* USER CODE END 0 */
+// Global variables for STM32cubeMonitor
+HAL_StatusTypeDef ret;
+uint8_t ret_ready = 10;
+uint8_t ret_accel = 20;
+uint8_t ret_exit_sleep = 20;
+uint8_t done_reset_func = 0;
+
+int16_t AccelX, AccelY, AccelZ;
+float AccelX_g, AccelY_g, AccelZ_g;
+
+// Initialize MPU
+void MPU6050_Init() {
+  ret = HAL_I2C_IsDeviceReady(&hi2c1, MPU6050_ADDR, 1, 100);
+
+  if (ret == HAL_OK) {
+    ret_ready = 10;
+  } else if (ret == HAL_ERROR) {
+    ret_ready = 11;
+    I2C_Bus_Recovery();
+  } else if (ret == HAL_BUSY) {
+    ret_ready = 12;
+    I2C_Bus_Recovery();
+  } else {
+    ret_ready = 13;
+    I2C_Bus_Recovery();
+  }
+
+  uint8_t temp_data = FS_ACC_4G;
+  ret = HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, REG_CONFIG_ACC, 1, &temp_data, 1, HAL_MAX_DELAY);
+
+  if (ret == HAL_OK) {
+    ret_accel = 20;
+  } else if (ret == HAL_ERROR) {
+    ret_accel = 21;
+    I2C_Bus_Recovery();
+  } else if (ret == HAL_BUSY) {
+    ret_accel = 22;
+    I2C_Bus_Recovery();
+  } else {
+    ret_accel = 23;
+    I2C_Bus_Recovery();
+  }
+
+  // Exit sleep by writing 0x00 to PWR_MGMT_1 register 0x6B
+  uint8_t data = 0;
+  ret = HAL_I2C_Mem_Write(&hi2c1, MPU6050_ADDR, REG_USR_CTRL, 1, &data, 1, HAL_MAX_DELAY);
+
+  if (ret == HAL_OK) {
+    ret_exit_sleep = 30;
+  } else if (ret == HAL_ERROR) {
+    ret_exit_sleep = 31;
+  } else if (ret == HAL_BUSY) {
+    ret_exit_sleep = 32;
+  } else {
+    ret_exit_sleep = 33;
+  }
+}
+
+// Resolve HAL_BUSY error
+void I2C_Bus_Recovery(void) {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  // Reconfigure SCL and SDA as GPIO
+  __HAL_RCC_GPIOB_CLK_ENABLE(); // Enable GPIO clock (adjust based on your pin)
+  GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9; // Replace with your I2C pins
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  // Manually clock SCL line
+  for (int i = 0; i < 21; i++) {
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET); // SCL High
+    HAL_Delay(20); // Short delay
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET); // SCL Low
+    HAL_Delay(20);
+  }
+
+  // Reinitialize I2C
+  HAL_I2C_DeInit(&hi2c1);
+  HAL_I2C_Init(&hi2c1);
+}
+
+
+void MPU6050_Read_Accel(int16_t *AccelX, int16_t *AccelY, int16_t *AccelZ) {
+  uint8_t buffer[6]; // Buffer to store the raw data (6 bytes for X, Y, Z)
+
+  // Read 6 bytes starting from ACCEL_XOUT_H (0x3B)
+  if (HAL_I2C_Mem_Read(&hi2c1, MPU6050_ADDR, ACCEL_XOUT_H, I2C_MEMADD_SIZE_8BIT, buffer, 6, HAL_MAX_DELAY) == HAL_OK) {
+    // Combine high and low bytes for each axis
+    *AccelX = (int16_t)(buffer[0] << 8 | buffer[1]); // X-axis
+    *AccelY = (int16_t)(buffer[2] << 8 | buffer[3]); // Y-axis
+    *AccelZ = (int16_t)(buffer[4] << 8 | buffer[5]); // Z-axis
+  } else {
+    // If reading fails, set default values or handle error
+    *AccelX = 0;
+    *AccelY = 0;
+    *AccelZ = 0;
+
+  }
+}
+
+float MPU6050_Convert_to_g(int16_t raw_value) {
+  return (float)raw_value / 16384.0;  // Assuming default ±2g sensitivity
+}
+//* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
-
-volatile HAL_StatusTypeDef rethi2c1;
-volatile HAL_StatusTypeDef rethi2c3;
-
 int main(void)
 {
 
@@ -106,21 +207,8 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_I2C1_Init();
-  MX_I2C3_Init();
-  MX_SPI2_Init();
-  MX_SPI3_Init();
   /* USER CODE BEGIN 2 */
-  // From stm32f4xx_hal_i2c.h
-  //HAL_StatusTypeDef HAL_I2C_IsDeviceReady(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint32_t Trials, uint32_t Timeout);
-  //  typedef enum
-  //  {
-  //    HAL_OK       = 0x00U,
-  //    HAL_ERROR    = 0x01U,
-  //    HAL_BUSY     = 0x02U,
-  //    HAL_TIMEOUT  = 0x03U
-  //  } HAL_StatusTypeDef;
-  rethi2c1 = HAL_I2C_IsDeviceReady(&hi2c1, (DEVICE_ADDRESS<<1)+0, 1, 100);
-  rethi2c3 = HAL_I2C_IsDeviceReady(&hi2c3, (DEVICE_ADDRESS<<1)+0, 1, 100);
+  MPU6050_Init();
 
   /* USER CODE END 2 */
 
@@ -131,13 +219,16 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    rethi2c1 = HAL_I2C_IsDeviceReady(&hi2c1, (DEVICE_ADDRESS<<1)+0, 1, 100);
-    rethi2c3 = HAL_I2C_IsDeviceReady(&hi2c3, (DEVICE_ADDRESS<<1)+0, 1, 100);
+    MPU6050_Read_Accel(&AccelX, &AccelY, &AccelZ);
+
+    AccelX_g = MPU6050_Convert_to_g(AccelX);
+    AccelY_g = MPU6050_Convert_to_g(AccelY);
+    AccelZ_g = MPU6050_Convert_to_g(AccelZ);
 
     // LED BLINKING
     HAL_GPIO_TogglePin (GPIOA, GPIO_PIN_5);
-    HAL_Delay (1000);   // Delay 100 ms
   }
+
   /* USER CODE END 3 */
 }
 
@@ -203,7 +294,7 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.ClockSpeed = 100000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
@@ -218,116 +309,6 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
-
-}
-
-/**
-  * @brief I2C3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C3_Init(void)
-{
-
-  /* USER CODE BEGIN I2C3_Init 0 */
-
-  /* USER CODE END I2C3_Init 0 */
-
-  /* USER CODE BEGIN I2C3_Init 1 */
-
-  /* USER CODE END I2C3_Init 1 */
-  hi2c3.Instance = I2C3;
-  hi2c3.Init.ClockSpeed = 400000;
-  hi2c3.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c3.Init.OwnAddress1 = 0;
-  hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c3.Init.OwnAddress2 = 0;
-  hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C3_Init 2 */
-
-  /* USER CODE END I2C3_Init 2 */
-
-}
-
-/**
-  * @brief SPI2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI2_Init(void)
-{
-
-  /* USER CODE BEGIN SPI2_Init 0 */
-
-  /* USER CODE END SPI2_Init 0 */
-
-  /* USER CODE BEGIN SPI2_Init 1 */
-
-  /* USER CODE END SPI2_Init 1 */
-  /* SPI2 parameter configuration*/
-  hspi2.Instance = SPI2;
-  hspi2.Init.Mode = SPI_MODE_MASTER;
-  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi2.Init.NSS = SPI_NSS_SOFT;
-  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi2.Init.CRCPolynomial = 10;
-  if (HAL_SPI_Init(&hspi2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SPI2_Init 2 */
-
-  /* USER CODE END SPI2_Init 2 */
-
-}
-
-/**
-  * @brief SPI3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI3_Init(void)
-{
-
-  /* USER CODE BEGIN SPI3_Init 0 */
-
-  /* USER CODE END SPI3_Init 0 */
-
-  /* USER CODE BEGIN SPI3_Init 1 */
-
-  /* USER CODE END SPI3_Init 1 */
-  /* SPI3 parameter configuration*/
-  hspi3.Instance = SPI3;
-  hspi3.Init.Mode = SPI_MODE_MASTER;
-  hspi3.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi3.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi3.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi3.Init.NSS = SPI_NSS_SOFT;
-  hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-  hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi3.Init.CRCPolynomial = 10;
-  if (HAL_SPI_Init(&hspi3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SPI3_Init 2 */
-
-  /* USER CODE END SPI3_Init 2 */
 
 }
 
